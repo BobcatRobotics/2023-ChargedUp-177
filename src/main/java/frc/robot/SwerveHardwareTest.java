97a56d6efab466d35b18909293ddfcb7b2a5a4f4
Cheekibreekiski <aidenkeegan278@gmail.com>
2023-01-14 08:41:10 -0500

    updated verndordeps [AK]

--- src/main/java/frc/robot/SwerveHardwareTest.java
+++ src/main/java/frc/robot/SwerveHardwareTest.java
@@ -2,11 +2,13 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot;
 
 
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+// import com.ctre.phoenix.motorcontrol.can.TalonFX;
+// import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.sensors.CANCoder;
 import edu.wpi.first.wpilibj2.command.CommandBase;
