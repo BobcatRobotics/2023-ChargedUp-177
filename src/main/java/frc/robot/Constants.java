b2d7be8e8e8aad125a3611b756da385910601ecf
ImReallyCravingACheesePizzaRightNow <ad95873486@gmail.com>
2023-01-14 13:26:46 -0500

    swerve mostly working lets goooo WIP [AT + AK + DH]

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -74,9 +74,9 @@ public final class Constants {
 
         /* Drive Motor Characterization Values 
          * Divide SYSID values by 12 to convert from volts to percent output for CTRE */
-        public static final double driveKS = (0.32 / 12); //TODO: This must be tuned to specific robot
-        public static final double driveKV = (1.51 / 12);
-        public static final double driveKA = (0.27 / 12);
+        public static final double driveKS = (0.47309 / 12); // TUNED
+        public static final double driveKV = (0.66261 / 12);
+        public static final double driveKA = (0.071697 / 12);
 
         /* Swerve Profiling Values */
         /** Meters per Second */
