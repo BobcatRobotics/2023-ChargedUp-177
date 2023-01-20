58ff87eb47e42cef80c46d36651f6715cf128dd6
Aiden Keegan <92986733+Cheekibreekiski@users.noreply.github.com>
2023-01-20 18:31:12 +0000

    added odometry printout [AK]

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -120,7 +120,11 @@ public class Swerve extends SubsystemBase {
         for(SwerveModule mod : mSwerveMods){
             SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Cancoder", mod.getCanCoder().getDegrees());
             SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Integrated", mod.getDistanceMeters());
-            SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Velocity", mod.getState().speedMetersPerSecond);    
+            SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Velocity", mod.getState().speedMetersPerSecond); 
         }
+        
+        SmartDashboard.putString("odometry", "x: " + swerveOdometry.getPoseMeters().getX() + " y: " + swerveOdometry.getPoseMeters().getY() + " rot: " + swerveOdometry.getPoseMeters().getRotation().getDegrees());
+
+
     }
 }
