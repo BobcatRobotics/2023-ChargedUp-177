8010c3334d4a0710a72f515eb6f29e253eaadc70
zainlok06 <zainlok06@gmail.com>
2023-02-02 18:43:33 -0500

    from elevator testing

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,6 +9,9 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.Constants.elevator;
+import frc.robot.subsystems.Elevator;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -28,6 +31,12 @@ public class Robot extends TimedRobot {
   private Timer timer;
 
   private int i = 0;
+  int state = 0;
+
+  private XboxController gamepad;
+  //private Elevate elevate;
+  private Elevator elevator;
+  private double d = 0;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -103,7 +112,41 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    
+    elevator.getBottomLimit();
+    elevator.getMiddleLimit();
+    elevator.getTopLimit();
+    state = elevator.get_state();
+    if (gamepad.getPOV() == Constants.elevator.D_Pad_Up){
+      if (state == -1 || state == 0){
+        d = 0.45;
+        state++;
+      }
+    }
+    else if (gamepad.getPOV() == Constants.elevator.D_Pad_Down){
+      if (state == 1 || state == 0){
+        state--;
+        d=-0.45;
+      }
+    }
+    if (d> -1 && d<1){
+      while (!elevator.getBottomLimit() || !elevator.getTopLimit()){
+        elevator.elevatorMotorset(d);
+      }
+    }
+    else{
+      if (d > 0){
+        d=1;
+        elevator.elevatorMotorset(d);
+      }
+      else{
+        d=-1;
+        elevator.elevatorMotorset(d);//motor sets
+      }
+    }
+
+  }
 
   @Override
   public void testInit() {
