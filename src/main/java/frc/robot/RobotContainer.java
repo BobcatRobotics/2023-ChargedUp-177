7b592f16b063db7440c7d7e7e0b5e424aed83fbd
HasiP145 <93354656+TBortlolan@users.noreply.github.com>
2023-02-18 10:21:44 -0500

    Revert "tried waitcommand but failed"
    This reverts commit e2b1b415b944bac4fbcd268fdd5d107740b0d23f.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,7 +12,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -79,7 +78,6 @@ public class RobotContainer {
         autoChooser.addOption("PathPlanner Test w/ Events", new SequentialCommandGroup(s_Swerve.followTrajectoryCommand()));
         SmartDashboard.putData(autoChooser);
         eventMap.put("chargeStation", align);
-        eventMap.put("waitCommand", new WaitCommand(2));
         // Configure the button bindings
         configureButtonBindings();
         m_Limelight.initializeLimeLight();
