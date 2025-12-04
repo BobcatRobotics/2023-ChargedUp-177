470e5a2a209d2342302a69e695146d3b9b81866f
ao <accent-truffle.9w@icloud.com>
2025-12-04 18:34:56 -0500

    Initial rebuild

---
+++ comp/Template/src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "template";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 7;
+  public static final String GIT_SHA = "31e303131ebfd5699a1aa5bcc434eb608c5825e0";
+  public static final String GIT_DATE = "2025-12-04 17:37:43 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-12-04 18:32:43 EST";
+  public static final long BUILD_UNIX_TIME = 1764891163325L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
