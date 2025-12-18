d01d8db74d51a0ea43e2a2eb94254a07ab722e17
h3L1X <116317505+h3L1X@users.noreply.github.com>
2025-12-18 18:50:21 -0500

    Finished intake code

--- comp/2025/src/main/java/frc/robot/BuildConstants.java
+++ comp/2025/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "template";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 210;
-  public static final String GIT_SHA = "5a89a4078e8669edba331e7d66cc18fff2b83e46";
-  public static final String GIT_DATE = "2025-12-10 23:44:10 EST";
+  public static final int GIT_REVISION = 211;
+  public static final String GIT_SHA = "371b3f4242d1180e22d83e1672189a25b89b03a8";
+  public static final String GIT_DATE = "2025-12-11 21:49:04 EST";
   public static final String GIT_BRANCH = "monorepo";
-  public static final String BUILD_DATE = "2025-12-11 21:33:30 EST";
-  public static final long BUILD_UNIX_TIME = 1765506810497L;
+  public static final String BUILD_DATE = "2025-12-18 18:48:44 EST";
+  public static final long BUILD_UNIX_TIME = 1766101724946L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- comp/2025/src/main/java/frc/robot/RobotContainer.java
+++ comp/2025/src/main/java/frc/robot/RobotContainer.java
@@ -21,6 +21,8 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 // import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
@@ -177,6 +179,11 @@ public class RobotContainer {
                 controller.getButton("B").onTrue(Commands.runOnce(
                                 () -> drive.setPose(new Pose2d(drive.getPose().getTranslation(), new Rotation2d())),
                                 drive).ignoringDisable(true));
+
+                controller.getButton("Y").whileTrue(new RunCommand(()-> intake.runIntakeOut()))
+                .onFalse(new InstantCommand(()-> intake.stop()));
+                
+        
         }
 
         /**
